b537f11b6bc0c3940d0ddd0ca4d138a372c8f841
ken2201 <ndayishimiyeken22@gmail.com>
2023-11-21 03:23:34 -0800

    adding the putchar using a buffer fonction that able to print and use a temporary storage to print a character

---
+++ _putchar.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* _putchar - write the character d to stdout
+* @d: the character to print
+*
+* Return: on success 1.
+* On error, -1 is returned .
+*/
+
+int _putchar(char d)
+{
+	return (buffer(d));
+}
+
+/**
+ * buffer - save a char in a buffer
+ * @d: the char
+ *
+ * Return: 1
+ **/
+int buffer(char d)
+{
+	static char buffering[1024];
+	static int m;
+
+	if (d == -1 || m == 1024)
+	{
+		write(1, buffering, m);
+		m = 0;
+	}
+
+	if (d != -1)
+		buffering[m++] = d;
+
+	return (1);
+}
