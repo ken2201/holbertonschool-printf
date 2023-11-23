5edf96187dad0dfe76d586ecfbabd99b2b179feb
ken2201 <ndayishimiyeken22@gmail.com>
2023-11-23 10:59:15 -0800

    correcting the integer function

--- print_integer.c
+++ print_integer.c
@@ -5,30 +5,32 @@
  * @args: variadic list of arguments
  * Return: the number of characters printed
  */
-int print_integer(va_list args) 
+int print_integer(va_list args)
 {
 	int num = va_arg(args, int);
-    	int count = 0;
-    	char buffer[12]; 
+	int count = 0;
+	char buffer[12];
 
-    	if (num < 0)
+	if (num < 0)
 	{
-       		count += write(1, "-", 1);
-      		num = -num;
+		count += write(1, "-", 1);
+		num = -num;
 	}
-
-   
-    	do
+	while (num > 0)
 	{
 		buffer[count++] = num % 10 + '0';
-        	num /= 10;
+		num /= 10;
 	}
-	while (num > 0);
 
-    	while (count > 0) 
+	if (count == 0)
 	{
-        	write(1, &buffer[--count], 1);
+		buffer[count++] = '0';
 	}
 
-    	return (count);
+	while (count > 0)
+	{
+		write(1, &buffer[--count], 1);
+	}
+
+	return (count);
 }
